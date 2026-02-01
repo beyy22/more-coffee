403ab92d05cf27cc7930816eb1ab85049688028b
More Coffee Dev <dev@morecoffee.com>
2026-02-02 02:52:25 +0700

    Add DB setup route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/install-db', function () {
+    $key = request()->query('key');
+    if ($key !== 'rahasia123') {
+        abort(403, 'Unauthorized');
+    }
+    
+    \Illuminate\Support\Facades\Artisan::call('migrate:fresh --seed --force');
+    return 'Database berhasil di-reset dan di-isi! Admin: admin@more.test / password';
+});
